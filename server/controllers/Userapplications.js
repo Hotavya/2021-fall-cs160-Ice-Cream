7559897fb5bf6b62712e09cbf852959324a576c9
Jing Wu <xiaojing.wu@sjsu.edu>
2021-10-24 22:59:43 -0700

    fix db connection

--- server/controllers/Userapplications.js
+++ server/controllers/Userapplications.js
@@ -1,29 +1,24 @@
-const Express = require("express");
-const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-const CONNECTION_URL = "mongodb+srv://dbUser:<icecream2021>@trackercluster.5gtu6.mongodb.net:27017";
-const DATABASE_NAME = "myFirstDatabase";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-
-var app = Express();
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
-var database, collection;
+var app = express();
+var db, collection
 
 app.listen(5000, () => {
-    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    mongoose.connect(process.env.DATABASE, function(error, client) {
         if(error) {
             throw error;
         }
-        database = client.db(DATABASE_NAME);
-        collection = database.collection("jobboard");
-        console.log("Connected to `" + DATABASE_NAME + "`!");
+        db = mongoose.connection;
+        collection = db.collection("useraccounts");
+        console.log("Connected to `" + process.env.DATABASE_NAME + "`!");
     });
 });
 
 app.get("/jobs", (request, response) => {
-    collection.find({}).toArray((error, result) => {
+    collection.find({}).toArray((result, error) => {
         if(error) {
             return response.status(500).send(error);
         }
