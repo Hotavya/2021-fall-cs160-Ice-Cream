bcd8823c7782511f4d83b25e2dd2c847344a4b19
Ben Mahanloo <benmahanloo@Bens-MacBook-Pro.local>
2021-10-25 11:47:01 -0700

    Sprint 4

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,58 @@
 import UserAccount from '../database/schemas/userAccount.js';
+import * as EmailValidator from 'email-validator';
 
-//TODO:This is just for feasability test, write robust implementation later.
+// Sign up
 export const signup = async (req, res) => {
-  const { username, email } = req.body;
-  try {
-    const newUserAccount = new UserAccount({ username, email });
-    await newUserAccount.save();
-  } catch (error) {
-    return res.status(500).json({
-      error: 'Internal Server Error',
-      message: 'Something went wrong in the server',
+  const { email, password } = req.body;
+  // Check if user left email or password field empty
+  if(!email || !password) {
+    return res.status(400).json({
+      message: "Email and password are required"
     });
   }
-  return res.status(200).send({ message: 'This signup was successfull' });
+  // Check if email is already used by an account
+  else if(await UserAccount.exists({ email: email })) {
+    return res.status(400).json({
+      message: 'Account already exists'
+    });
+  }
+  // Check if email entered is correct format
+  else if(!EmailValidator.validate(email)) {
+    return res.status(400).json({
+      message: "Invalid email address"
+    });
+  }
+  // Check if user password is 8 or more characters in length
+  else if(password.length < 8) {
+    return res.status(400).json({
+      message: "Password must be at least 8 characters in length"
+    });
+  }
+  // Create Account
+  else {
+    try {
+      const newUserAccount = new UserAccount({ email, password });
+      await newUserAccount.save();
+      //res.redirect('/auth/signin');
+    } catch (error) {
+      return res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Account not created. Something went wrong in the server'
+      });
+    }
+    return res.status(200).send({ message: 'Account Successfully Created' });
+  }
+};
+
+// Sign in
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  UserAccount.findOne({email}, (err, user) => {
+    if(err || !email) {
+      return res.status(400).json({
+        message: "Account does not exist with these credentials"
+      })
+    }
+  })
 };
--- server/database/schemas/userAccount.js
+++ server/database/schemas/userAccount.js
@@ -1,16 +1,32 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
 
-//TODO:This is just for feasability test, revise this.
-const userAccountSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+/* User account schema */
+const userAccountSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+  );
+
+// Hash user password before storing in database
+userAccountSchema.pre('save', async function(next) {
+  try{
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+})
 
 const UserAccount = mongoose.model('UserAccount', userAccountSchema);
 export default UserAccount;
