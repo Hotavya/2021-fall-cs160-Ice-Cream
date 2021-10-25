8554a50c519d0e589fd20ab676c02cb8080a389c
Jing Wu <xiaojing.wu@sjsu.edu>
2021-10-24 20:40:05 -0700

    add user API endpoint

---
+++ server/controllers/Userapplications.js
@@ -0,0 +1,32 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb+srv://dbUser:<icecream2021>@trackercluster.5gtu6.mongodb.net:27017";
+const DATABASE_NAME = "myFirstDatabase";
+
+
+var app = Express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("jobboard");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.get("/jobs", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
