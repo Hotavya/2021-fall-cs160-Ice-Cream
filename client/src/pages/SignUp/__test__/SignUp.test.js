41f9ad26ee0a15a5e736dea1ac6d04f296b3f9f9
JoeKabongo <jonathankabongo23@gmail.com>
2021-11-11 12:54:26 -0800

    put file in test folder

--- client/src/pages/SignUp/__test__/SignUp.test.js
+++ client/src/pages/SignUp/__test__/SignUp.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignupPage from './SignUp';
+import SignupPage from '../SignUp';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
